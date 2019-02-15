5e056427b0a5219ab1254c924f1e38c56ffa6185
wuqibuhuan <zhangdongqi@example.com>
2019-02-15 22:10:35 +0800

    侧边栏结构完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -10,13 +10,82 @@
         </el-col>
         <el-col :span="1">
           <div class="grid-content bg-purple">
-            <a href="#" class="loginout">退出</a>
+            <a href="#" class="loginout" @click.prevent="handleLoginout()">退出</a>
           </div>
         </el-col>
       </el-row>
     </el-header>
   <el-container>
-    <el-aside class="aside" width="200px">Aside</el-aside>
+      <!-- 导航 -->
+    <el-aside class="aside" width="200px">
+        <el-menu
+      default-active="1"
+      :router="true"
+      unique-opened>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>用户管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1"><i class="el-icon-menu"></i>用户列表</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+
+      <!-- 权限管理 -->
+       <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>权限管理</span>
+        </template>
+        
+          <el-menu-item index="1-1"><i class="el-icon-menu"></i>角色列表</el-menu-item>
+           <el-menu-item index="1-1"><i class="el-icon-menu"></i>权限列表</el-menu-item>
+      </el-submenu>
+
+      <!-- 商品管理 -->
+       <el-submenu index="4">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>商品管理</span>
+        </template>
+        <el-menu-item-group>
+        
+          <el-menu-item index="1-1"><i class="el-icon-menu"></i>商品列表</el-menu-item>
+           <el-menu-item index="1-1"><i class="el-icon-menu"></i>分类参数</el-menu-item>
+            <el-menu-item index="1-1"><i class="el-icon-menu"></i>商品分类</el-menu-item>
+         
+        </el-menu-item-group>
+      </el-submenu>
+
+      <!-- 订单管理 -->
+       <el-submenu index="5">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>订单管理</span>
+        </template>
+        <el-menu-item-group>
+        
+          <el-menu-item index="1-1"><i class="el-icon-menu"></i>订单列表</el-menu-item>
+         
+        </el-menu-item-group>
+      </el-submenu>
+
+      <!-- 数据统计 -->
+       <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>数据统计</span>
+        </template>
+        <el-menu-item-group>
+        
+          <el-menu-item index="1-1"><i class="el-icon-menu"></i>数据列表</el-menu-item>
+         
+        </el-menu-item-group> 
+      </el-submenu>
+     
+    </el-menu>
+    </el-aside>
     <el-main class="main">Main</el-main>
   </el-container>
 </el-container>
@@ -24,17 +93,36 @@
 
 <script>
 export default {
-
+    beforeMount() {
+    if (!localStorage.getItem("token")) {
+      this.$router.push({
+        name: "login"
+      });
+    }
+  },
+  methods: {
+    // 退出
+    handleLoginout() {
+      // 1. 清除token
+      localStorage.clear();
+      // 2. 跳转到login
+      this.$router.push({
+        name: "login"
+      });
+      // 提示
+      this.$message.warning("退出成功");
+    }
+  }
 }
 </script>
 
 <style>
 .container {
     height: 100%;
-    background-color: green;
+    background-color: rgb(217, 230, 217);
 }
 .aside {
-    background-color: gold;
+    /* background-color: gold; */
 }
 .main {
     background-color: fuchsia;
